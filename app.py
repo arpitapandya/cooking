a42524fd2fe2c370f03598637c682d7543a0fc81
Arpita <arpya74@gmail.com>
2021-02-16 10:48:20 -0500

    few changes

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
 
+
 toolbar = DebugToolbarExtension(app)
 connect_db(app)
 db.create_all()
